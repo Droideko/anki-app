9bf44f9fc9899848d039c8d1851b7e21e4b52d2c
andrii <andrii.chapni@gmail.com>
2026-01-11 18:30:56 +0100

    Refactor RootLayout component by removing unused code and comments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Portal } from 'react-native-paper';
 import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
 
@@ -11,68 +10,9 @@ import { useLanguageStore } from '@shared/store/useLanguageStore';
 import Providers from '@shared/contexts/providers';
 import { useThemeColor } from '@shared/hooks/useThemeColor';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-// SplashScreen.setOptions({
-//   duration: 3000,
-//   fade: true,
-// });
-
 export default function RootLayout() {
-  // useEffect(() => {
-  //   GoogleSignin.configure({
-  //     iosClientId: "YOUR_IOS_CLIENT_ID.apps.googleusercontent.com", // Ваш iOS Client ID
-  //     webClientId: "YOUR_WEB_CLIENT_ID.apps.googleusercontent.com", // Ваш Web Client ID
-  //     offlineAccess: true,
-  //   });
-  // }, []);
-  // Somewhere in your code
-
-  // const signIn = async () => {
-  //   try {
-  //     await GoogleSignin.hasPlayServices();
-  //     const { type, data: userData } = await GoogleSignin.signIn();
-
-  //     if (type === "cancelled") {
-  //       return;
-  //     }
-
-  //     // Получаем idToken и отправляем на сервер
-  //     // const { idToken } = userInfo;
-  //     const response = await fetch("https://your-backend.com/auth/google", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({ idToken: userData.idToken }),
-  //     });
-
-  //     if (!response.ok) {
-  //       throw new Error("Ошибка при аутентификации");
-  //     }
-
-  //     const data = await response.json();
-  //     // Сохраните accessToken и refreshToken, например, в SecureStore или AsyncStorage
-  //     console.log("Access Token:", data.accessToken);
-  //     console.log("Refresh Token:", data.refreshToken);
-  //     Alert.alert("Успех", "Вход выполнен успешно");
-  //   } catch (error: any) {
-  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-  //       // Пользователь отменил вход
-  //       Alert.alert("Отмена", "Вход отменен пользователем");
-  //     } else if (error.code === statusCodes.IN_PROGRESS) {
-  //       // Вход уже в процессе
-  //       Alert.alert("В процессе", "Вход уже выполняется");
-  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-  //       // Сервисы Google Play недоступны
-  //       Alert.alert("Ошибка", "Google Play Services недоступны");
-  //     } else {
-  //       // Другая ошибка
-  //       console.error(error);
-  //       Alert.alert("Ошибка", "Произошла ошибка при входе");
-  //     }
-  //   }
-  // };
-
   const [loaded] = useFonts({
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
